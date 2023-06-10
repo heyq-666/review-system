5f47615bd985c29aed87248f28ce784529ce8c67
wangweiqi.08 <wangweiqi.08@bytedance.com>
2023-06-10 17:50:08 +0800

    解决小程序端与管理后台鉴权冲突问题

--- jeecg-boot-base-core/src/main/java/org/jeecg/config/shiro/ShiroConfig.java
+++ jeecg-boot-base-core/src/main/java/org/jeecg/config/shiro/ShiroConfig.java
@@ -76,6 +76,7 @@ public class ShiroConfig {
             }
         }
         // 配置不会被拦截的链接 顺序判断
+        filterChainDefinitionMap.put("/reviewFront/**", "anon"); //放行小程序端请求
         filterChainDefinitionMap.put("/sys/cas/client/validateLogin", "anon"); //cas验证登录
         filterChainDefinitionMap.put("/sys/randomImage/**", "anon"); //登录验证码接口排除
         filterChainDefinitionMap.put("/sys/checkCaptcha", "anon"); //登录验证码接口排除
